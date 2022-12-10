640626d80fb2830c2a0c324f760b4c9286d92408
Rubenjr1999 <113224289+Rubenjr1999@users.noreply.github.com>
2022-12-10 23:54:50 +0800

    Add Calendar

--- Main_Project/teacher/assets/header.view.php
+++ Main_Project/teacher/assets/header.view.php
@@ -41,9 +41,9 @@
     <div class="container-fluid" id="header">
         <div class="row">
             <nav class="navbar navbar-expand-md navbar-top">
-                <div class="container-fluid">
+                <div class="container-fluid ms-5">
                     <!-- Left -->
-                    <img src="images/HFA-Logo.png" alt="Logo" width="50" height="50">
+                    <img src="images/HFA-Logo.png" alt="Logo" width="40" height="40">
                     <a href="teacher.php" class="navbar-brand fs-3 page-title">Dashboard</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTop"
                         aria-controls="navbarTop" aria-expanded="false" aria-label="Toggle navigation">
@@ -54,11 +54,11 @@
                     <div class="collapse navbar-collapse" id="navbarTop">
                         <ul class="navbar-nav ms-auto me-0">
                             <li class="nav-item">
-                                <a href="student.announcement.php" class="nav-link"><i
+                                <a href="teacher.announcement.php" class="nav-link"><i
                                         class="fa-solid fa-bullhorn"></i></a>
                             </li>
                             <li class="nab-item">
-                                <a href="student.calendar.php" class="nav-link"><i
+                                <a href="teacher.calendar.php" class="nav-link"><i
                                         class="fa-regular fa-calendar"></i></a>
                             </li>
                             <li class="nav-item">
@@ -71,7 +71,7 @@
                             <li class="nav-item dropdown">
 
                                 <a href="" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">User </a>
+                                    aria-expanded="false"></a>
                                 <ul class="dropdown-menu  dropdown-menu-end">
 
                                     <li><a href="teacher.profile.php" class="dropdown-item">profile</a></li>
